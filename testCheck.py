5a101dc5f025de7bc43d27c56ce33b04b62c5848
Geraldo Luiz <geraldorj-2010@hotmail.com>
2022-01-13 13:48:44 -0300

    Deleted files

--- testCheck.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-child = subprocess.Popen(['python3', 'chess.py', '-cligui'], stdin=subprocess.PIPE)
-#child.communicate(bytes('d2d4\nd7d5\nc1g5\ne7e6\nb1c3\nd8g5\nd1d3\nh7h6\ne1c1',
-child.communicate(bytes('e2e4\nf7f5\nd1h5\n', "utf-8"))
